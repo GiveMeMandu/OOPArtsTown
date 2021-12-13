f1a3e7649ab012fbf39f6e2263eeb8f9f4edb385
김우찬 <52920226+GiveMeMandu@users.noreply.github.com>
2021-12-13 16:40:23 +0900

    network: initial Settings
    Offline : Main Menu
    Online : OOPArts Town
    Player : OOPArts Town Citizen Player

--- Assets/UI/Scripts/CreateRoomUI.cs
+++ Assets/UI/Scripts/CreateRoomUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Mirror;
 
 public class CreateRoomUI : MonoBehaviour
 {
@@ -13,6 +14,15 @@ public class CreateRoomUI : MonoBehaviour
     public void UpdateMaxPlayerCount(int count)
     {
         roomData.maxPlayerCount = count;
+    }
+
+    public void CreateRoom()
+    {
+        var manager = OOPArtsTownNetworkManager.singleton;
+        // 방 설정 작업 처리
+        //
+        // 생성 및 참가
+        manager.StartHost();
     }
 }
 
